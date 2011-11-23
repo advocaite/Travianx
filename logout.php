ab53b69682b7997cac552ac5ea2d25f0ede5cf6e
GameEngine <paul.navalassie@gmail.com>
2011-11-23 09:25:55 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,4 @@
 <?php
-
 #################################################################################
 ##              -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE =-                 ##
 ## --------------------------------------------------------------------------- ##
@@ -9,51 +8,9 @@
 ##  Copyright:     TravianX (c) 2010-2011. All rights reserved.                ##
 ##                                                                             ##
 #################################################################################
-
-
 include("GameEngine/Account.php");
 $start = $generator->pageLoadTimeStart();
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-	<head>
-		<title><?php echo SERVER_NAME; ?></title>
-		<meta name="content-language" content="en" />
-		<meta http-equiv="cache-control" content="max-age=0" />
-		<meta http-equiv="imagetoolbar" content="no" />
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<script src="mt-core.js?2389c" type="text/javascript"></script>
-
-		<script src="mt-more.js?2389c" type="text/javascript"></script>
-		<script src="unx.js?2389c" type="text/javascript"></script>
-		<script src="new.js?2389c" type="text/javascript"></script>
-	<link href="<?php echo GP_LOCATE; ?>lang/en/lang.css?f4b7c" rel="stylesheet" type="text/css" />
-	<link href="<?php echo GP_LOCATE; ?>lang/en/compact.css?f4b7c" rel="stylesheet" type="text/css" />
-	<?php
-	if($session->gpack == null || GP_ENABLE == false) {
-	echo "
-	<link href='".GP_LOCATE."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".GP_LOCATE."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
-	} else {
-	echo "
-	<link href='".$session->gpack."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".$session->gpack."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
-	}
-	?>
-	<script type="text/javascript">
-
-		window.addEvent('domready', start);
-	</script>
-</head>
- 
- 
-<body class="v35 ie ie8">
-<div class="wrapper">
-<img style="filter:chroma();" src="img/x.gif" id="msfilter" alt="" />
-<div id="dynamic_header">
-	</div>
-<div id="header"></div>
-
+include("Templates/header.tpl");?>
 <div id="mid">
 <?php include("Templates/menu.tpl"); ?>
 		<div id="content"  class="logout">
@@ -69,23 +26,5 @@ include("Templates/news.tpl");
 </div>
 <div class="clear"></div>
 </div>
-<div class="footer-stopper"></div>
-<div class="clear"></div>
-
-<?php 
-include("Templates/footer.tpl"); 
-?>
-<div id="stime">
-<div id="ltime">
-<div id="ltimeWrap">
-Calculated in <b><?php
-echo round(($generator->pageLoadTimeEnd()-$start)*1000);
-?></b> ms
- 
-<br />Server time: <span id="tp1" class="b"><?php echo date('H:i:s'); ?></span>
-</div>
-	</div>
-</div>
-<div id="ce"></div>
-</body>
-</html>
+<?php
+include("Templates/footer.tpl");
